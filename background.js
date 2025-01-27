653fe2201eb6bb4cf0ebbaaa74ff99e7916955fa
Akhil Pasupuleti <pletiakhil100@gmail.com>
2025-01-27 19:51:46 +0530

    add overlay for sidebar

--- background.js
+++ background.js
@@ -26,11 +26,11 @@ function toggleSidebar() {
           position: fixed;
           top: 0;
           right: 0;
-          width: 300px;
+          width: 320px;
           height: 100%;
           background-color: white;
           box-shadow: -2px 0 5px rgba(0, 0, 0, 0.1);
-          z-index: 1000;
+          z-index: 99999;
           padding: 20px;
           overflow-y: auto;
         }
@@ -40,6 +40,6 @@ function toggleSidebar() {
     `;
 
     document.body.appendChild(div);
-    document.body.style.marginRight = "300px";
+    // document.body.style.marginRight = "300px";
   }
 }
